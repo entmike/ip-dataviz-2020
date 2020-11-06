cc1b1cc723169c8ace8751adeba957c027d4242b
Mike Howles <entmike@gmail.com>
2020-11-06 14:50:40 -0600

    GMap example

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -34,6 +34,12 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    [
+      'nuxt-gmaps',
+      {
+        key: 'AIzaSyBnJ5jl0UqbTRJqe1uJEVj_J3OmZTRQRkc',
+      },
+    ],
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,15 +1,87 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <!-- FYI - I think this module is trash - Mike -->
+  <GMap
+    ref="gMap"
+    class="dataviz"
+    :cluster="{ options: { styles: clusterStyle } }"
+    :center="home"
+    :options="{
+      fullscreenControl: true,
+      streetViewControl: false,
+      mapTypeControl: true,
+      zoomControl: true,
+      // Mikey want scroll wheely
+      // gestureHandling: 'cooperative',
+      styles: mapStyle,
+    }"
+    :zoom="10"
+  >
+    <GMapMarker
+      v-for="location in locations"
+      :key="location.id"
+      :position="{ lat: location.lat, lng: location.lng }"
+      :options="{
+        icon: location === currentLocation ? pins.selected : pins.notSelected,
+      }"
+      @click="currentLocation = location"
+    >
+      <GMapInfoWindow :options="{ maxWidth: 200 }">
+        <div style="font-size: 13px; color: #006699; text-alignment: left">
+          <b>{{ location.name }}</b>
+          <br />
+          <br />
+          <code>
+            Lat: {{ location.lat }},
+            <br />
+            Lng: {{ location.lng }}
+          </code>
+        </div>
+      </GMapInfoWindow>
+    </GMapMarker>
+    <GMapCircle :options="circleOptions" />
+  </GMap>
 </template>
+<script>
+export default {
+  name: 'DataViz',
+  data() {
+    return {
+      // San Francisco
+      home: { lat: 37.7749, lng: -122.4194 },
+      currentLocation: {},
+      circleOptions: {},
+      // Ideally, this would be fed/filtered by backend API called in Axios served by Express so we don't put a zillion rows in at once
+      locations: [
+        {
+          id: 1,
+          lat: 37.71603882,
+          lng: -122.4402551,
+          type: 'Robbery',
+          name: 'Some Crime',
+        },
+      ],
+      // We can base-64/host static icons for this.
+      pins: {
+        robbery: 'some url or base64 mime',
+        larceny: 'some other url',
+      },
+      mapStyle: [],
+      clusterStyle: [
+        {
+          url:
+            'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m1.png',
+          width: 56,
+          height: 56,
+          textColor: '#fff',
+        },
+      ],
+    }
+  },
+}
+</script>
+<style lang="scss">
+.dataviz .GMap__Wrapper {
+  width: 100%;
+  height: 800px;
+}
+</style>
